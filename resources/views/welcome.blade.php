2a1cd11a769fd4b41036c8eab33c32c8ade38d85
rajesh06 <abhi.crushefix@gmail.com>
2018-02-12 11:40:52 +0600

    Secure asset added for last commit

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -7,13 +7,13 @@
     <meta name="csrf-token" content="{{ csrf_token() }}">
     <meta http-equiv="Expires" content="30" />
     <title>পিপীলিকা (সার্চ ইঞ্জিন)</title>
-    <link rel="shortcut icon" type="image/x-icon" href="{{asset('img/favicon.ico')}}"/>
+    <link rel="shortcut icon" type="image/x-icon" href="{{ secure_asset('img/favicon.ico')}}"/>
     <meta name="description" content="পিপীলিকা বাংলাদেশের প্রথম এবং একমাত্র সার্চ ইঞ্জিন যা বাংলা ও ইংরেজী দুই ভাষাতেই কাজ করতে সক্ষম। এই উন্মুক্ত ওয়েব সার্ভিসটি সারা দেশের সাম্প্রতিক গ্রহণসাধ্য তথ্য অনুসন্ধান করতে সহায়তা করে।
      এটি দেশের প্রধান বাংলা ও ইংরেজী পত্রিকার সংবাদ, বাংলা ব্লগ, বাংলা উইকিপিডিয়া ও সরকারি তথ্য স্বয়ংক্রিয়ভাবে বিশ্লেষণ ও সংরক্ষণ করে। জনপ্রিয় সার্চ ইঞ্জিনগুলোর কোনটিতেই বাংলা ভাষার উপর তেমন গুরুত্ব আরোপ করা হয়নি। তাই আমরা বাংলা তথ্য
      বিশ্লেষণ ও অনুসন্ধানের উপর গুরুত্ব দেয়ার চেষ্টা করেছি।" />
     <meta name="keywords" content="pipilika, পিপীলিকা, search, অনুসন্ধান, bangladesh, বাংলাদেশ, dhaka, ঢাকা,  business, ব্যবসা ও বাণিজ্য, politics, sports, খেলাধূলা, technology, তথ্য ও
     প্রযুক্তি,  health, national infokosh, bangla, ই-তথ্যকোষ,  news, সংবাদ, bengali, companies, bangla spell check, bangla search engine, বাংলা সার্চ ইঞ্জিন" />
-    <link href="{{ asset('fonts/fonts.css') }}" rel="stylesheet" type="text/css"/>
+    <link href="{{ secure_asset('fonts/fonts.css') }}" rel="stylesheet" type="text/css"/>
     <link href="{{ mix('css/app.css') }}" rel="stylesheet" type="text/css"/>
     <script>
         window.ga_key = '{{ env('GOOGLE_ANALYTICS_KEY') }}';
@@ -30,7 +30,7 @@
     </div>
 </div>
 
-<script src="{{ asset('js/rangy/rangy-core.min.js')}}"></script>
+<script src="{{ secure_asset('js/rangy/rangy-core.min.js')}}"></script>
 <script src="{{ mix('js/app.js')}}" ></script>
 </body>
 </html>
